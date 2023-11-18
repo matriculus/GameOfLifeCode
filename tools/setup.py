c5bd33e954f76b076d1229c63cad2e2cebdf0291
Vishnu Lex <pradeeshvishnu@outlook.com>
2023-11-18 02:37:19 +0000

    fix: pipeline

--- tools/setup.py
+++ tools/setup.py
@@ -4,10 +4,4 @@ setup(
     name="tools",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[
-        "numpy",
-        "Pillow",
-        "pygame",
-        "pyinstaller",
-    ],
 )
